13d483a7df1643ba07fdfcdc072210b298f048e0
Elisabeth Engel <Elisabeth.Engel@comsysto.com>
2013-08-14 18:15:58 +0200

    Draggable expample added

---
+++ js/default.js
@@ -0,0 +1,9 @@
+function dragUser(user, event) {
+    event.dataTransfer.setData('User', user.id);
+}
+
+
+function dropUser(target, event) {
+    var user = event.dataTransfer.getData('User');
+    //target.appendChild(document.getElementById(user));
+}
